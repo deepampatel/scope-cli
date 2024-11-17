94e8e17cfdac6302d47261f9a63212a3f90f1cf4
Deepam Patel <deepam8155@gmail.com>
2024-11-17 15:25:15 +0400

    chore: version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="scope-cli",
-    version="0.2.2",
+    version="0.2.3",
     description="A CLI tool for visualizing directory sizes and checking port usage.",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
